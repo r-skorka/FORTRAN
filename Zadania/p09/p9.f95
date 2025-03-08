5e57464891c0bcc13d82b94fdd8536c4ef8d0f19
Radomir Skórka <147660435+r-skorka@users.noreply.github.com>
2025-03-08 15:51:03 +0100

    ćwiczenia 2 - 10.03

---
+++ Zadania/p09/p9.f95
@@ -0,0 +1,28 @@
+PROGRAM p9
+IMPLICIT NONE
+
+INTERFACE
+	REAL FUNCTION cube_root (x)
+		REAL :: x
+	END FUNCTION cube_root
+END INTERFACE
+
+REAL :: a,b
+
+PRINT *, "typea number:"
+READ *, a
+
+b=cube_root(a)
+PRINT *, "cube root of ", a, " is ", b
+
+STOP
+END PROGRAM p9
+
+REAL FUNCTION cube_root (x)
+IMPLICIT NONE
+
+REAL :: x,log_x
+log_x = LOG(x)
+cube_root = EXP(log_x/3.0)
+
+END FUNCTION cube_root
