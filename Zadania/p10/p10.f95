a8bd089616ee830cb24ff9f7ac0fc85b0b9f4353
Radomir Skórka <147660435+r-skorka@users.noreply.github.com>
2025-03-16 22:48:00 +0100

    ćwiczenia 3 - 17.03

---
+++ Zadania/p10/p10.f95
@@ -0,0 +1,48 @@
+program p10
+implicit none
+
+	integer :: i, j, p, q, r, x, n, m
+
+	print *, "start=1, stop10, increment=1 (domyslny)"
+	do i=1, 10
+		print*, i
+	end do
+	print *
+	
+	print *, "start=20, stop=50, increment=5"
+	do j=20, 50, 5
+		print *, j
+	end do
+	print *
+	
+	print *, "start=7, stop=19, increment=4"
+	do p=7, 19, 4
+		print *, p
+	end do
+	print *
+	
+	print *, "start=4, stop=5, increment=6"
+	do q=4, 5, 6
+		print *, q
+	end do
+	print *
+	
+	print *, "start=6, stop=5, increment=4"
+	do r=6, 5, 4
+		print *, r
+	end do
+	print *
+	
+	print *, "start=25, stop=0, increment=-5"
+	do x=25, 0, -5
+		print *, x
+	end do
+	print *
+	
+	print *, "start=20, stop=-20, increment=-6"
+	do m=20, -20, -6
+		print *, m
+	end do
+	print*
+stop
+end program p10
